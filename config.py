2bc30695f6bdbb1abd970184d8d95de6810ed128
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-08-09 23:16:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,10 +56,10 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
 
 # Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "shrinkforearn.in") # shortlink domain without https://
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "https://shrinkforearn.in/") # shortlink domain without https://
 SHORTLINK_API = environ.get("SHORTLINK_API", "e6657b00e3f201daba5e3351cd611143ce9127a2") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+RsQwXu9PiiQ1MjNl") # how to open link 
 
