625a0e68b504d966c7f566bc91482db11b0e9e85
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-08-10 12:58:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,12 +59,12 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "https://shrinkforearn.in/") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "e6657b00e3f201daba5e3351cd611143ce9127a2") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "publicearn.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "63abbd0502e32cb99ea2d82edf518d903c827e3f") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+RsQwXu9PiiQ1MjNl") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://arpitgavla121.blogspot.com/2024/08/adult-video_2.html") # For More Information Check Video On Yt - @Tech_VJ
