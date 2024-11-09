a85f4fb2ba02cf4f5130581150811a1da4fd8d4c
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-11-09 10:07:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -64,7 +64,7 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "63abbd0502e32cb99ea2d82edf518d903c
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+RsQwXu9PiiQ1MjNl") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://latestadultleak.blogspot.com/2024/08/adult-video_2.html") # For More Information Check Video On Yt - @Tech_VJ
