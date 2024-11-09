95435197085c7a73d5faaaa739bafe7f13307f4e
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-11-09 09:59:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,10 +56,10 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
 
 # Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "publicearn.com") # shortlink domain without https://
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "primeurl.in") # shortlink domain without https://
 SHORTLINK_API = environ.get("SHORTLINK_API", "63abbd0502e32cb99ea2d82edf518d903c827e3f") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+RsQwXu9PiiQ1MjNl") # how to open link 
 
