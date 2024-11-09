2c4aa9bb3baa069dd50542e5a30592abd73a3f8a
MovieBots <154045205+MovieBots@users.noreply.github.com>
2024-11-09 12:12:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,8 +59,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "anlinks.in") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "f23a7deb684fd9af4867329c36b8ae7f27113c77") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "shortxlinks.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "5f720133a8f53a7299c058d5b8ec918bd899f4f1") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+RsQwXu9PiiQ1MjNl") # how to open link 
 
 # Website Info:
